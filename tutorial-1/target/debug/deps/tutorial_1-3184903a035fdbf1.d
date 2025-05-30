6778908a88039d92136810ec4c7428e3d1ef146a
Shivam Sahni <shivamsahni507@gmail.com>
2025-05-30 02:10:05 +0000

    add tutorial one

---
+++ tutorial-1/target/debug/deps/tutorial_1-3184903a035fdbf1.d
@@ -0,0 +1,5 @@
+/mnt/d/Rust/tutorial-1/target/debug/deps/tutorial_1-3184903a035fdbf1: src/main.rs
+
+/mnt/d/Rust/tutorial-1/target/debug/deps/tutorial_1-3184903a035fdbf1.d: src/main.rs
+
+src/main.rs:
---
+++ tutorial-1/target/debug/tutorial-1.d
@@ -0,0 +1,1 @@
+/mnt/d/Rust/tutorial-1/target/debug/tutorial-1: /mnt/d/Rust/tutorial-1/src/main.rs
